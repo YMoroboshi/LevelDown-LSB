b55d303a5f88634cbe6d7219dd42b404238bb48f
YMoroboshi <ruisu7@hotmail.com>
2026-02-07 05:09:24 -0500

    Update aldo.lua
    Updated Aldo's lua file to reflect his behavior on retail.
    
    Also added a DEX boost and TP Bonus (though this can be removed if it's too much).

--- scripts/actions/spells/trust/aldo.lua
+++ scripts/actions/spells/trust/aldo.lua
@@ -13,7 +13,20 @@ spellObject.onSpellCast = function(caster, target, spell)
 end
 
 spellObject.onMobSpawn = function(mob)
-    xi.trust.message(mob, xi.trust.messageOffset.SPAWN)
+    xi.trust.teamworkMessage(mob, {
+        [xi.magic.spell.LION] = xi.trust.messageOffset.TEAMWORK_1,
+		[xi.magic.spell.ZEID] = xi.trust.messageOffset.TEAMWORK_2,
+		[xi.magic.spell.GILGAMESH] = xi.trust.messageOffset.TEAMWORK_3,
+    })
+
+	mob:setTrustTPSkillSettings(ai.tp.CLOSER_UNTIL_TP, ai.s.RANDOM, 2000)
+	mob:addGambit(ai.t.SELF, { ai.c.TP_GTE, 1000 }, { ai.r.JA, ai.s.SPECIFIC, xi.ja.ASSASSINS_CHARGE })
+	mob:addGambit(ai.t.SELF, { ai.c.STATUS, xi.effect.ASSASSINS_CHARGE }, {ai.r.WS, ai.s.RANDOM, 0 })
+	mob:addGambit(ai.t.TARGET, { ai.c.ALWAYS, 0 }, { ai.r.JA, ai.s.SPECIFIC, xi.ja.BULLY })
+	mob:addGambit(ai.t.TARGET, { ai.c.STATUS, xi.effect.DOUBT }, { ai.r.JA, ai.s.SPECIFIC, xi.ja.SNEAK_ATTACK })
+
+	mob:addMod(xi.mod.DEX, 200)
+	mob:addMod(xi.mod.TP_BONUS, 250)
 end
 
 spellObject.onMobDespawn = function(mob)
